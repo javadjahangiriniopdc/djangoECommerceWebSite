3493a11a023e056021b8b249ac5d3f807a969857
javadjahangiri <javad.jahangiri.niopcd@gmail.com>
2022-03-17 11:09:57 +0330

    add models Category,Brand,Color,Size

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from main.models import *
+
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Color)
+admin.site.register(Size)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'brand', 'color', 'size', 'price', 'status')
+    list_editable = ('status',)
+
+
+admin.site.register(Product,ProductAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+# Category
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="cat_imgs/")
+
+    def __str__(self):
+        return self.title
+
+
+# Brand
+class Brand(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="brand_imgs/")
+
+    def __str__(self):
+        return self.title
+
+
+# Color
+class Color(models.Model):
+    title = models.CharField(max_length=100)
+    color_code = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+# Size
+class Size(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+# product Model
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="product_imgs/")
+    slug = models.CharField(max_length=400)
+    detail = models.TextField()
+    specs = models.TextField()
+    price = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
