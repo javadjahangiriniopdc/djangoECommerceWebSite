4319a993703bd3133a4e40374f408d4a34ca0903
javadjahangiri <javad.jahangiri.niopcd@gmail.com>
2022-03-17 12:11:25 +0330

    add models ProductAttributeAdmin

--- main/admin.py
+++ main/admin.py
@@ -10,8 +10,15 @@ admin.site.register(Size)
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'brand', 'color', 'size', 'price', 'status')
+    list_display = ('id', 'title', 'brand', 'color', 'size', 'status')
     list_editable = ('status',)
 
 
-admin.site.register(Product,ProductAdmin)
+admin.site.register(Product, ProductAdmin)
+
+
+class ProductAttributeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'color', 'size', 'price')
+
+
+admin.site.register(ProductAttribute, ProductAttributeAdmin)
--- main/models.py
+++ main/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+# banner
+class Banner(models.Model):
+    img = models.CharField(max_length=200)
+    alt_text = models.CharField(max_length=300)
+
+
 # Category
 class Category(models.Model):
     title = models.CharField(max_length=100)
@@ -43,12 +49,22 @@ class Product(models.Model):
     slug = models.CharField(max_length=400)
     detail = models.TextField()
     specs = models.TextField()
-    price = models.PositiveIntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     color = models.ForeignKey(Color, on_delete=models.CASCADE)
     size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     status = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
+
+
+# Product Attribute
+class ProductAttribute(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.product.title
