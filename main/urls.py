eda7931ccafd374e1b01fc8033999efa0b23dfd5
javadjahangiri <javad.jahangiri.niopcd@gmail.com>
2022-03-18 18:31:20 +0330

    fetch category_list

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,13 @@ from django.urls import path
 
 from main import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('category-list', views.category_list, name='category-list'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Category
+
 
 # Create your views here.
 def home(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
+
+# category
+def category_list(request):
+    data = Category.objects.all().order_by('-id')
+    return render(request, 'category_list.html', {'data': data})
