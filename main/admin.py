17e17b879ff6d99560973d7a82be9d6d46a6a16d
javadjahangiri <javad.jahangiri.niopcd@gmail.com>
2022-03-18 19:45:44 +0330

    Customize Django Admin Panel

--- main/admin.py
+++ main/admin.py
@@ -3,10 +3,25 @@ from django.contrib import admin
 # Register your models here.
 from main.models import *
 
-admin.site.register(Category)
+admin.site.register(Banner)
+
 admin.site.register(Brand)
-admin.site.register(Color)
+
 admin.site.register(Size)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'image_tag')
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ('title', 'color_bg')
+
+
+admin.site.register(Color, ColorAdmin)
 
 
 class ProductAdmin(admin.ModelAdmin):
--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.html import mark_safe
 
 
 # banner
@@ -6,11 +7,20 @@ class Banner(models.Model):
     img = models.CharField(max_length=200)
     alt_text = models.CharField(max_length=300)
 
+    class Meta:
+        verbose_name_plural = "1. Banners"
+
 
 # Category
 class Category(models.Model):
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to="cat_imgs/")
+
+    class Meta:
+        verbose_name_plural = '2. Categories'
+
+    def image_tag(self):
+        return mark_safe('<img src="%s" width="50" height="50" />' % self.image.url)
 
     def __str__(self):
         return self.title
@@ -21,6 +31,9 @@ class Brand(models.Model):
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to="brand_imgs/")
 
+    class Meta:
+        verbose_name_plural = '3. Brands'
+
     def __str__(self):
         return self.title
 
@@ -29,6 +42,12 @@ class Brand(models.Model):
 class Color(models.Model):
     title = models.CharField(max_length=100)
     color_code = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = '4. Colors'
+
+    def color_bg(self):
+        return mark_safe('<div style="width:30px;height:30px;background-color:%s"></div>' % self.color_code)
 
     def __str__(self):
         return self.title
@@ -40,6 +59,9 @@ class Size(models.Model):
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name_plural = '5. Sizes'
 
 
 # product Model
@@ -55,6 +77,9 @@ class Product(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     status = models.BooleanField(default=True)
 
+    class Meta:
+        verbose_name_plural = '6. Products'
+
     def __str__(self):
         return self.title
 
@@ -68,3 +93,6 @@ class ProductAttribute(models.Model):
 
     def __str__(self):
         return self.product.title
+
+    class Meta:
+        verbose_name_plural = '7. ProductAttribute'
