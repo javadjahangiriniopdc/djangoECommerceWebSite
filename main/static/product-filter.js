11db48aa8aff70a2a94bfad6196abf6fdea0639f
javadjahangiri <javad.jahangiri.niopcd@gmail.com>
2022-04-01 19:04:02 +0430

    Filter-products-with-jquery-ajax-with-price

--- main/static/product-filter.js
+++ main/static/product-filter.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
     $(".ajaxLoader").hide();
-    $(".filter-checkbox").on('click', function () {
+    $(".filter-checkbox,#priceFilterBtn").on('click', function () {
         var _filterObj = {};
+        var _minPrice = $('#maxPrice').attr('min');
+        var _maxPrice = $('#maxPrice').val();
+        _filterObj.minPrice=_minPrice;
+        _filterObj.maxPrice=_maxPrice;
         $(".filter-checkbox").each(function (index, ele) {
             var _filterVal = $(this).val();
             var _filterKey = $(this).data('filter');
@@ -20,26 +24,26 @@ $(document).ready(function () {
             },
             success: function (res) {
                 console.log(res);
-                 $(".ajaxLoader").hide();
+                $(".ajaxLoader").hide();
                 $("#filteredProducts").html(res.data);
 
             }
         });
 
     });
-	// Filter Product According to the price
-	$("#maxPrice").on('blur',function(){
-		var _min=$(this).attr('min');
-		var _max=$(this).attr('max');
-		var _value=$(this).val();
-		console.log(_value,_min,_max);
-		if(_value < parseInt(_min) || _value > parseInt(_max)){
-			alert('Values should be '+_min+'-'+_max);
-			$(this).val(_min);
-			$(this).focus();
-			$("#rangeInput").val(_min);
-			return false;
-		}
-	});
-	// End
+    // Filter Product According to the price
+    $("#maxPrice").on('blur', function () {
+        var _min = $(this).attr('min');
+        var _max = $(this).attr('max');
+        var _value = $(this).val();
+        console.log(_value, _min, _max);
+        if (_value < parseInt(_min) || _value > parseInt(_max)) {
+            alert('Values should be ' + _min + '-' + _max);
+            $(this).val(_min);
+            $(this).focus();
+            $("#rangeInput").val(_min);
+            return false;
+        }
+    });
+    // End
 });
